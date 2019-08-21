96b7def71464df43c4fd0cda4970036d1c9ed4ab
ghkhrshch <george.khruschov@gmail.com>
2019-08-21 18:39:49 +0300

    Solution added

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -7,24 +7,49 @@ package core.basesyntax;
  */
 public class Rectangle {
     private Integer width;
-    private Integer length;
+    private Integer height;
     private String color;
 
-    public Rectangle(Integer width, Integer length, String color) {
+    public Rectangle(Integer width, Integer height, String color) {
         this.width = width;
-        this.length = length;
+        this.height = height;
         this.color = color;
     }
 
-    public Integer getWidth() {
+    public Integer getA() {
         return width;
     }
 
-    public Integer getLength() {
-        return length;
+    public Integer getB() {
+        return height;
     }
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Rectangle) {
+            Rectangle rectangleObj = (Rectangle) obj;
+            if (!this.width.equals(rectangleObj.width)) {
+                return false;
+            }
+            if (!this.height.equals(rectangleObj.height)) {
+                return false;
+            }
+            if (!this.color.equals(rectangleObj.color)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (width + height) * 31 - (color.length() * height);
     }
 }
